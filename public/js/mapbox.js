19b42300f8179513d84c9e7d7e808f97a780e3e7
João Pedro Telles Fava <88334985+jaypeter19@users.noreply.github.com>
2026-05-14 16:39:04 -0300

    feat: mapbox pop up for each location improved

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -8,6 +8,45 @@ const mapboxScriptUrl = new URL(document.currentScript.src);
 const geojsonUrl = new URL('../json/geojson.json', mapboxScriptUrl);
 const isMobile = window.matchMedia('(max-width: 575px)').matches;
 
+const escapeHtml = (value = '') => String(value).replace(/[&<>"']/g, (character) => ({
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#39;'
+}[character]));
+
+const getPopupImageUrl = (properties) => {
+    const imageUrl = (
+        properties.imageUrl
+        || properties.image
+        || properties.backgroundImage
+        || properties.background_image
+        || ''
+    );
+
+    return imageUrl
+        ? new URL(imageUrl, mapboxScriptUrl).href
+        : '';
+};
+
+const getPopupHtml = (properties) => {
+    const title = escapeHtml(properties.title);
+    const description = escapeHtml(properties.description).replace(/[.,;:]+$/, '');
+    const imageUrl = getPopupImageUrl(properties);
+    const imageStyle = imageUrl
+        ? ` style="--map-popup-image: url('${escapeHtml(imageUrl)}');"`
+        : '';
+
+    return `
+        <article class="map-popup-card">
+            <h3 class="map-popup-title">${title}</h3>
+            <div class="map-popup-image"${imageStyle}>
+                <span class="map-popup-location">${description}</span>
+            </div>
+        </article>
+    `;
+};
 
 const map = new mapboxgl.Map({
     container: 'map',
@@ -34,10 +73,12 @@ fetch(geojsonUrl)
             new mapboxgl.Marker(el)
                 .setLngLat(feature.geometry.coordinates)
                 .setPopup(
-                    new mapboxgl.Popup({ offset: 25 })
-                        .setHTML(
-                            `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
-                        )
+                    new mapboxgl.Popup({
+                        offset: 25,
+                        closeButton: false,
+                        className: 'ip-map-popup'
+                    })
+                        .setHTML(getPopupHtml(feature.properties))
                 )
                 .addTo(map);
         }
